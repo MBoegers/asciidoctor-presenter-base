4deadf14c66613225ab6abb2134a55575fae67b4
Merlin Bögershausen <merlin.boegershausen@rwth-aachen.de>
2021-12-16 15:11:25 +0100

    hard wire reveal.js viar attribute and root file name to index

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
-for talk in ./src/*/main.adoc ; do
- talk_dir="${talk::-10}"
- echo "Building $talk_dir and link reveal.js"
- ./bin/asciidoctor-revealjs-linux $talk
- #mkdir "$talk_out_dir/reveal.js"
- cp -r -f ./bin/reveal.js "$talk_dir/reveal.js"
+for talk in ./src/*/index.adoc ; do
+ echo "Building ${talk::-10}"
+ ./bin/asciidoctor-revealjs-linux $talk --attribute revealjsdir=../../bin/reveal.js/
 done
 echo "Build talks build"
